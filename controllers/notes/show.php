86bf54b52bbd15c42f0b6680308139a070e0a386
GuyGabriell <jonahgabriel85@yahoo.com>
2024-08-20 11:17:09 +0100

    it was hardcoded bcos i am yet to review auth

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -9,14 +9,12 @@ $config = require base_path('config.php');
 
 $db = new Database($config['database']);
 
+
 $currentUserId = 1;
 
 
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
 
-    //dd($_POST);
-
-   //form was submitted delete the current note.
    $note = $db->query('select * from notes where id = :id', [
 
     'id' => $_GET['id']
@@ -27,16 +25,28 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     authorize($note['user_id'] === $currentUserId);
 
 
-    $db->query('delete from note where id = :id', [
+    $db->query('delete from notes where id = :id', [
+
         'id' => $_GET['id']
     ]);
 
+    header('location: /notes');
+    exit();
+
+} 
+else {
+
+   $note = $db->query('select * from notes where id = :id', [
+
+    'id' => $_GET['id']
+
+    ])->findOrFail();
+
 }
 
-//dd($_SERVER);
 
 view("notes/show.view.php", [
-
+ 
     'heading' => 'Note',
 
     'note' => $note
